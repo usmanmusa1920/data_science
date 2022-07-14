c3a83d271958562dfcf17899f28fbd03f667b375
Usman Musa <usmanmusa1920@gmail.com>
2022-07-14 01:10:25 +0100

    first commit

---
+++ intro_data.py
@@ -0,0 +1,21 @@
+"""
+    DATA SCIENCE
+    
+    In this content we are going to take a journey into data science world along with various techniques used in this field and mostly we will be using python programming language together with some of it external libraries like MATPLOTLIB, NUMPY, etc to tackle our need.
+    
+    Data science is the process of deriving knowledge and insight from a huge and diverse set of data through organizing, processing and analysing the data. It involves many different discipline like mathematical and statisticalmodelling, extracting data from it source and applying data visualisation techniques. Often it also involves handling big data techniques to gather both structured and unsturctured data. We are going to use jupyter notebook for this chapter, so you have to install jupyter notebook system, and also some python libraries (matplotlib and numpy).
+    
+      import numpy as np
+      import matplotlib.pyplot as plt
+
+
+      m = np.array(range(0,20,2))
+      n = np.array([5,8,0,'h',5,3,1,0,6,2])
+
+      plt.ylabel('The y-axis is n')
+      plt.xlabel('The x-axis is m')
+      plt.legend(title='My analysis', loc='upper left')
+      plt.bar(m,n,color='hotpink')
+      plt.draw()
+
+"""
